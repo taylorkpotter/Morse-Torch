52200c39a98bebe7505bc63e06f5c87dd1067afc
Taylor <taylorkpotter@me.com>
2014-04-14 13:48:53 -0700

    Initial Commit

---
+++ Morse/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Morse
+//
+//  Created by Taylor Potter on 4/14/14.
+//  Copyright (c) 2014 potter.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Morse/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Morse
+//
+//  Created by Taylor Potter on 4/14/14.
+//  Copyright (c) 2014 potter.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
